9ac270abf4a3b76c032acf5b9f0db83bcbe909b8
wagnercc <wagcamargocastilho@gmail.com>
2023-01-06 09:43:20 -0300

    First commit with builder integration.

---
+++ src/BuilderIo/CatchAllRoute.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import { BuilderComponent, builder, useIsPreviewing } from "@builder.io/react";
+
+// Put your API key here
+builder.init('your api key');
+
+// set whether you're using the Visual Editor,
+// whether there are changes,
+// and render the content if found
+export default function CatchAllRoute() {
+  const isPreviewingInBuilder = useIsPreviewing();
+  const [notFound, setNotFound] = useState(false);
+  const [content, setContent] = useState(null);
+
+  // get the page content from Builder
+  useEffect(() => {
+    async function fetchContent() {
+      const content = await builder
+        .get("page", {
+          url: window.location.pathname
+        })
+        .promise();
+
+      setContent(content);
+      setNotFound(!content);
+    }
+    fetchContent();
+  }, [window.location.pathname]);
+  
+  // If no page is found, return 
+  // a 404 page from your code.
+  // The following hypothetical 
+  // <FourOhFour> is placeholder.
+  if (notFound && !isPreviewingInBuilder) {
+    return <div>Not found!</div>
+  }
+
+  // return the page when found
+  return (
+    <>
+      <head>
+        <title>{content?.data.title}</title>
+      </head>
+      {/* Render the Builder page */}
+      <BuilderComponent model="page" content={content} />
+    </>
+  );
+}
---
+++ src/Components/CustomButton.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Builder } from "@builder.io/react";
+
+export const CustomButton = (props) => {
+    console.log('props', props)
+	return (
+		<button
+			style={{
+				backgroundColor: props.backgroundColor,
+				borderRadius: "8px",
+				fontFamily: "monospace",
+				color: "#FFF",
+                border: 0,
+                padding: '10px'
+			}}
+		>
+			Hey! {props.title}
+		</button>
+	);
+};
+
+// Make sure that every page where renders Builder
+// content calls the file containing this function call
+Builder.registerComponent(CustomButton, {
+	name: "CustomButton",
+	inputs: [
+        { name: "title", type: "text" },
+        { name: 'backgroundColor', type: 'color' }
+    ],
+	image: "https://tabler-icons.io/static/tabler-icons/icons-png/3d-cube-sphere-off.png",
+});
